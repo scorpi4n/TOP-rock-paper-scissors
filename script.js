86c40ed4395992b90a0f9e3f50df1bbd97e8bf58
James Phillips <thebugmanjames@gmail.com>
2022-05-18 16:52:50 -0500

    Create game() function

--- script.js
+++ script.js
@@ -28,20 +28,49 @@ function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay()
 
     if (playerSelection === computerSelection) {
-        return "It's a tie! Try again"
+        console.log("It's a tie! Try again")
+        return 'other'
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return 'You chose rock and I chose paper, so you lose.'
+        console.log('You chose rock and I chose paper, so you lose.')
+        return 'lose'
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return 'You chose rock and I chose scissors so you win! Keep it up!'
+        console.log('You chose rock and I chose scissors so you win! Keep it up!')
+        return 'win'
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return 'You chose paper and I chose rock so you win! Keep it up!'
+        console.log('You chose paper and I chose rock so you win! Keep it up!')
+        return 'win'
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return 'You chose paper and I chose scissors so you lose.'
+        console.log('You chose paper and I chose scissors so you lose.')
+        return 'lose'
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return 'You chose scissors and I chose rock so you lose.'
+        console.log('You chose scissors and I chose rock so you lose.')
+        return 'lose'
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return 'You chose scissors and I chose paper so you win! Keep it up!'
+        console.log('You chose scissors and I chose paper so you win! Keep it up!')
+        return 'win'
     } else if (playerSelection !== 'rock' || 'paper' || 'scissors') {
-        return 'Please enter rock, paper, or scissors.'
+        console.log('Please enter rock, paper, or scissors.')
+        return 'other'
+    }
+}
+
+// Create function game that keeps score in a five-round game
+function game() {
+
+    let playerScore = 0
+    let computerScore = 0
+
+    while (playerScore < 5 && computerScore < 5) {
+        roundResult = playRound()
+        
+        if (roundResult.includes('win')) {
+            playerScore++
+        } else if ( roundResult.includes('lose')) {
+            computerScore++
+        }
+
+        console.log(roundResult)
+        console.log(playerScore)
+        console.log(computerScore)
     }
 }
