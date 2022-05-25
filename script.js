58215cecbaa61fefd4cea0f28be0afa43c0b7fd3
James Phillips <thebugmanjames@gmail.com>
2022-05-25 18:34:43 -0500

    Create base UI and update index.html

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// HTML elements
+const resultsPara = document.getElementById('results')
+const winnerLoser = document.getElementById('winnerLoser')
+
 // comuterPlay() returns 'Rock', 'Paper', or 'Scissors' at random
 function computerPlay() {
     computerSelection = Math.random()
@@ -12,63 +16,93 @@ function computerPlay() {
         computerSelection = 'scissors'
         return computerSelection
     } else {
-        return 'An error occured. Please try again.'
+        resultsPara.innerText = 'An error occured. Please try again.'
     }
 }
 
 // playRound() plays one round of Rock Paper Scissors and returns a corresponding string
 function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt('Rock, paper, or scissors?', '')
-// playerSelection is made case-insensitive
-    playerSelection = playerSelection.trim()
-    playerSelection = playerSelection.toLowerCase()
-
     computerSelection = computerPlay()
 
     if (playerSelection === computerSelection) {
-        console.log("It's a tie! Try again")
+        resultsPara.innerText = ("It's a tie! Try again")
         return 'other'
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        console.log('You chose rock and I chose paper, so you lose.')
+        resultsPara.innerText = ('You chose rock and I chose paper, so you lose.')
         return 'lose'
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        console.log('You chose rock and I chose scissors so you win! Keep it up!')
+        resultsPara.innerText = ('You chose rock and I chose scissors so you win! Keep it up!')
         return 'win'
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        console.log('You chose paper and I chose rock so you win! Keep it up!')
+        resultsPara.innerText = ('You chose paper and I chose rock so you win! Keep it up!')
         return 'win'
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        console.log('You chose paper and I chose scissors so you lose.')
+        resultsPara.innerText = ('You chose paper and I chose scissors so you lose.')
         return 'lose'
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        console.log('You chose scissors and I chose rock so you lose.')
+        resultsPara.innerText = ('You chose scissors and I chose rock so you lose.')
         return 'lose'
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        console.log('You chose scissors and I chose paper so you win! Keep it up!')
+        resultsPara.innerText = ('You chose scissors and I chose paper so you win! Keep it up!')
         return 'win'
     } else if (playerSelection !== 'rock' || 'paper' || 'scissors') {
-        console.log('Please enter rock, paper, or scissors.')
+        resultsPara.innerText = ('Please enter rock, paper, or scissors.')
         return 'other'
     }
 }
 
+let playerScore = 0
+let computerScore = 0
+
+const rockBtn = document.getElementById('rock-button')
+const paperBtn = document.getElementById('paper-button')
+const scissorsBtn = document.getElementById('scissors-button')
+
+rockBtn.addEventListener('click', () => {
+    play('rock')
+    // playRound('rock')
+})
+paperBtn.addEventListener('click', () => {
+    play('paper')
+    // playRound('paper')
+})
+scissorsBtn.addEventListener('click', () => {
+    play('scissors')
+    // playRound('scissors')
+})
+
+
+
 // game() keeps score in a five-round game
-function game() {
-
-    let playerScore = 0
-    let computerScore = 0
-
+function play(choice) {
     while (playerScore < 5 && computerScore < 5) {
-        roundResult = playRound()
+        roundResult = playRound(choice)
         
-        if (roundResult.includes('win')) {
+        if (roundResult === 'win') {
             playerScore++
-        } else if ( roundResult.includes('lose')) {
+        } else if ( roundResult === 'lose') {
             computerScore++
         }
 
-        console.log(roundResult)
         console.log(playerScore)
         console.log(computerScore)
+
+        break
     }
+
+    if (playerScore === 5 || computerScore === 5) {
+
+        if (playerScore === 5) {
+            winnerLoser.innerText = 'Good game, you won!'
+            // playerScore = 0
+            // computerScore = 0
+        } else if (computerScore === 5) {
+            winnerLoser.innerText = 'You lost, play again?'
+            // playerScore = 0
+            // computerScore = 0
+        }
+        playerScore = 0
+        computerScore = 0
+    }
+
 }
